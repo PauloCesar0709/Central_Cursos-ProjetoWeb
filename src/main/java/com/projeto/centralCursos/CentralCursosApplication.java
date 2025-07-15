f89cd465a9901e40907c4716f54bbadbc21e789f
PauloCesar0709 <paulocsm2006@gmail.com>
2025-07-15 09:53:55 -0300

    Criação do Projeto

---
+++ src/main/java/com/projeto/centralCursos/CentralCursosApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.centralCursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/centralCursos/CentralCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.centralCursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
